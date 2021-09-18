77aa7324ac9d53fdc102d4c427320c5271483610
mazunki <rolferen@gmail.com>
2021-09-18 23:40:46 +0200

    Craeted some easier verbosity levels, argument support

--- Manager.py
+++ Manager.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python
 import configparser
 
+try:
+    from termcolor import colored
+except ImportError:
+    # if termcolor is not installed, strings should just be plain colourless
+    # installed via pip
+    def colored(*args, **kwargs):
+        return args[0]
+
+
 class Manager:
-    def __init__(self, keyname, configfile_name):
+    def __init__(self, keyname, configfile_name, verbosity=1):
         self.keyname = keyname
+        self.verbosity = verbosity
         self._read_config(configfile_name)
+
         self.packages = []
 
     def update_database(self):
         pass
 
     def _read_config(self, configfile_name):
+        self.debug(f"{configfile_name=}")
         self.config = configparser.RawConfigParser()
         self.config.read(configfile_name)
+
+    def debug(self, *args, **kwargs):
+        if self.verbosity >= kwargs.get("level", 1):
+            print(colored("[DEBUG/Manager]", "yellow"), *args, **kwargs)
 
     def has_package(self, pkg_name):
         return pkg_name in self.packages
--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 #!/usr/bin/py
 import xdg
-import os
+import os, sys
+import argparse
+try:
+    from termcolor import colored
+except ImportError:
+    # if termcolor is not installed, strings should just be plain colourless
+    # installed via pip
+    def colored(*args, **kwargs):
+        return args[0]
+    
 
 from Package import *
 from Manager import *
 
 
 class Crackle:
-    def __init__(self):
-        self.config_path = os.path.join(xdg.xdg_config_home(), "crackle")
-        print("config: ", self.config_path)
-        self.cache_path = os.path.join(xdg.xdg_cache_home(), "crackle")
+    def __init__(self, *args, **kwargs):
+        self.verbosity = 2 if "--verbose" in kwargs.keys() else 1
+        self.debug(f"{args=}, {kwargs=}")
+        self.config_path = kwargs.get("config", os.path.join(xdg.xdg_config_home(), "crackle"))
+        self.cache_path = kwargs.get("cache", os.path.join(xdg.xdg_cache_home(), "crackle"))
+
+        self.debug(f"{self.config_path=}")
+        self.debug(f"{self.cache_path=}")
+
         self._read_config(os.path.join(self.config_path, "crackle.conf"))
 
-        self.managers = ( Manager(name, os.path.join(self.config_path, name[1]+".conf")) for name in self.config.items("Managers") )
+        self.managers = ( Manager(name, os.path.join(self.config_path, name[1]+".conf"), verbosity=self.verbosity) for name in self.config.items("Managers") )
 
     def install(self, pkg_name):
         pkg = None
@@ -21,18 +35,31 @@ class Crackle:
             if manager.has_package(pkg_name):
                 pkg = Package(pkg_name, manager)
                 
-        print(pkg)
+        print(colored("[INSTALL]", "green"), pkg)
 
     def _read_config(self, configfile_name):
-        print(configfile_name)
+        self.debug(f"{configfile_name=}")
         self.config = configparser.RawConfigParser()
         self.config.read(configfile_name)
-        print(dict(self.config), "\n\n\n")
+
+    def debug(self, *args, **kwargs):
+        if self.verbosity >= kwargs.get("level", 1):
+            print(colored("[DEBUG]", "yellow"), *args, **kwargs)
 
 
-def main():
-    crackle = Crackle()
+
+def main(*args, **kwargs):
+    crackle = Crackle(*args, **kwargs)
     crackle.install("test")
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Install applications local to the user")
+    parser.add_argument("-c", "--config", type=str)
+    parser.add_argument("--cache", type=str)
+    parser.add_argument("-v", "--verbose", action="store_true")
+    options = parser.parse_args()
+    
+    args, kwargs = options._get_args(), dict(options._get_kwargs())
+
+    main(*args, **kwargs)
+
