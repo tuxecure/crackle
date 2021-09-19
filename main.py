aea653df49928d320b5ab5113dcd2ecdf9bb6494
mazunki <rolferen@gmail.com>
2021-09-19 03:34:00 +0200

    actually falling back to proper fallback values

--- main.py
+++ main.py
@@ -22,29 +22,32 @@ class Crackle:
 
         # Getting basepaths for Crackle to work with
         # Configuration defaults to ~/.config/crackle
-        self.config_path = kwargs.get("config") or os.path.join(xdg.xdg_config_home(), "crackle")
+        self.config_path = kwargs.get("config") or os.path.join(xdg.XDG_CONFIG_HOME, "crackle")
         self.debug(f"{self.config_path=}")
 
+        self._read_config(os.path.join(self.config_path, "crackle.conf"))
+
         # Cache defaults to ~/.cache/crackle. Used to store things we don't want to recalculate
-        self.cache_path = kwargs.get("cache") or os.path.join(xdg.xdg_cache_home(), "crackle") 
+        self.cache_path = kwargs.get("cache") or self.config.get("Options", "CRACKLE_CACHE_DIR") or os.path.join(xdg.XDG_CACHE_HOME, "crackle") 
         self.debug(f"{self.cache_path=}")
 
         # Data files are stored under ~/.local/share/crackle/src by default. We use this for our source code
-        self.src_path = kwargs.get("datapath") or os.path.join(xdg.xdg_data_home(), "crackle/src")
+        self.src_path = kwargs.get("datapath") or self.config.get("Options", "CRACKLE_SRC_DIR") or oos.path.join(xdg.XDG_DATA_HOME, "crackle/src")
         self.debug(f"{self.src_path=}")
 
         # Binary files built from source are found in ~/.local/share/crackle/bin by default. Add this to your $PATH
-        self.bin_path = kwargs.get("binpath") or os.path.join(xdg.xdg_data_home(), "crackle/bin")
+        self.bin_path = kwargs.get("binpath") or self.config.get("Options", "CRACKLE_BIN_DIR") or os.path.join(xdg.XDG_DATA_HOME, "crackle/bin")
         self.debug(f"{self.bin_path=}")
 
-        self._read_config(os.path.join(self.config_path, "crackle.conf"))
 
         self.managers = {*()}
         for mgr_name, mgr_conf in self.config.items("Managers"):
             config_path = mgr_conf if os.path.isabs(mgr_conf) else os.path.join(self.config_path, mgr_conf) 
+            fallback_bin_path = self.bin_path
+            fallback_src_path = os.path.join(self.src_path, mgr_name)
             self.debug(f"{config_path=}")
 
-            mgr = Manager(mgr_name, config_path, verbosity=self.verbosity, fallback_src_path=os.path.join(self.bin_path, mgr_conf))
+            mgr = Manager(mgr_name, config_path, verbosity=self.verbosity, fallback_src_path=fallback_src_path, fallback_bin_path=fallback_bin_path)
             self.managers.add(mgr)
 
     def install(self, pkg_name):
